cfef9859b7925913b60ce8d0d030cdc539bb3b86
July <belorusochkayul@gmail.com>
2020-08-04 16:49:56 +0300

    add tasks about simple classes

---
+++ Cars.java
@@ -0,0 +1,13 @@
+package by.kiparo.Cars;
+
+public class Cars {
+    String brand;
+    int tank_volume;
+    boolean transmission;
+
+    public Cars(String brand, int tank_volume, boolean transmission){
+        this.tank_volume = tank_volume;
+        this.brand = brand;
+        this.transmission = transmission;
+    }
+}
