dcf55831b7d8662dc03b9cac7414b3c432e138f0
July <belorusochkaYul@gmail.com>
2020-12-11 19:37:32 +0300

    Add task with API

---
+++ Fruit.java
@@ -0,0 +1,17 @@
+public class Fruit {
+    private String fruitName;
+    private int calories;
+    private float weight;
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit fruit = new Fruit();
+        try {
+            Class classFruit = fruit.getClass();
+
+            Field fieldString = classFruit.getDeclaredField("fruitName");
+            fieldString.setAccessible(true);
+            fieldString.set(fruit, "mango");
+
+            Field fieldFloat = classFruit.getDeclaredField("weight");
+            fieldFloat.setAccessible(true);
+            fieldFloat.setFloat(fruit, 220);
+
+            Field fieldInt = classFruit.getDeclaredField("calories");
+            fieldInt.setAccessible(true);
+            fieldInt.setInt(fruit, 145);
+
+            System.out.println("Информация о фрукте: " + fieldString.getName() + ":" + fruit.getFruitName() + " " + fieldInt.getName() + "=" + fruit.getWeight() + " " + fieldFloat.getName() + "=" + fruit.getCalories());
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
